89dbf6171960a50c0a695fda2eaa89ea51faa1f5
Pranjall Kumar <pranjallk1995@gmail.com>
2023-11-17 14:57:56 +0530

    Add files via upload

---
+++ span-transformation-visualization/config.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+# numeric
+GRID_SIZE = 5
+GRID_SPACING = 1
+
+# VECTORS
+INITIAL_BASIS = np.array([[1, 0], [0, 1]])
+TRANSFORMED_BASIS = np.array([[1, -2], [1, 0]])
+
+# colors
+BASIS_COLORS = ["#33cc33", "#006600"]
+GRID_COLOR = "#3399ff"
+AXIS_COLOR = "#ffcc66"
