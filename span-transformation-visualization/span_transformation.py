50b6d6b25329e97173328d30da26382b470e3fc5
Pranjall Kumar <pranjallk1995@gmail.com>
2023-11-17 13:42:07 +0530

    Update span_transformation.py

--- span-transformation-visualization/span_transformation.py
+++ span-transformation-visualization/span_transformation.py
@@ -60,22 +60,38 @@ class Grid():
         fig = self.plot_basis(basis, fig)
         fig.write_html(name)
 
-class TransformedBasis():
-
-    def __init__(self) -> None:
-        self.basis = np.array([[1, -2], [3, 0]])
-
-    def perform_transformation(self, vector: np.ndarray) -> np.ndarray:
-        return np.matmul(np.transpose(self.basis), vector)
-
 class InitialBasis():
 
     def __init__(self) -> None:
+        """
+        --> A set of 2 basis vectors are represented using the default unit vectors i_hat and j_hat.
+        --> Hence the span is the default XY 2D plane, as the two are linerly independent.
+        --> The i_hat and j_hat are [1, 0]^T and [0, 1]^T respectivly.
+        --> Hence the matrix will be [[1, 0], [0, 1]].
+        """
+        # basis vectors are represented in a list one after the other for easy of coding.
+        # here it looks same as the basis matrix.
         self.basis = np.array([[1, 0], [0, 1]])
         self.grid_cords = np.arange(-cfg.GRID_SIZE, cfg.GRID_SIZE+1, cfg.GRID_SPACING)
         self.grid = np.asarray(
             [[x_cord, y_cord] for y_cord in self.grid_cords for x_cord in self.grid_cords]
             ).reshape((2*cfg.GRID_SIZE+1, 2*cfg.GRID_SIZE+1, 2))
+        
+class TransformedBasis():
+
+    def __init__(self) -> None:
+        """
+        --> New basis vectors are introduced such that new_i_hat is [1, -2]^T and new_j_hat is [3, 0]^T.
+        --> Certain Linear Transformation was resposible for these new basis vectors which can be ignored for now.
+        --> Since the two new basis vectors are linearly independent, the span is a 2D plane again.
+        --> The matrix with these basis vectors is [[1, 3], [-2, 0]].
+        """
+        # basis vectors are represented in a list one after the other for easy of coding.
+        # here is the transpose will be the correct matrix representation of these basis vectors (see line 91).
+        self.basis = np.array([[1, -2], [3, 0]])
+
+    def perform_transformation(self, vector: np.ndarray) -> np.ndarray:
+        return np.matmul(np.transpose(self.basis), vector)
 
 if __name__ == "__main__":
 
