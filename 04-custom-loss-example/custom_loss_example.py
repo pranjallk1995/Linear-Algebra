374e5d1216d7b23a353aa42f3f1717d8d7de0850
Pranjall Kumar <pranjallk1995@gmail.com>
2024-05-10 12:41:26 +0530

    Update custom_loss_example.py

--- 04-custom-loss-example/custom_loss_example.py
+++ 04-custom-loss-example/custom_loss_example.py
@@ -1,3 +1,4 @@
+import json
 import tensorflow as tf
 
 from tensorflow.keras.layers import Dense
@@ -8,7 +9,12 @@ def custom_loss(y_true, y_pred, extra_parameter=10):
 
 # make model
 model = tf.keras.Sequential()
-model.add(Dense(1, activation='sigmoid', input_shape=(1,)))
+model.add(
+    Dense(
+        units=1, activation='sigmoid', input_shape=(2,),                                           # Notice the input shape
+        kernel_initializer='glorot_normal', bias_initializer='glorot_normal'
+    )
+)
 
 # check model
 print(model.summary())
@@ -17,17 +23,17 @@ print(model.summary())
 model.compile(loss=custom_loss)
 
 # train model
-x = tf.convert_to_tensor([1.])
+x = tf.convert_to_tensor([[1., 1.]])                                                              # Notice the x shape compared to x
 y = tf.convert_to_tensor([0.])
 
 model.fit(x, y)
 
 # check:
-weight = tf.convert_to_tensor(model.layers[0].get_weights()[0])
+weights = tf.convert_to_tensor(model.layers[0].get_weights()[0])
 bias = tf.convert_to_tensor(model.layers[0].get_weights()[1])
 
-linear_composition = weight[0]*x + bias
+linear_composition = weights[0]*x[0][0] + weights[1]*x[0][1] + bias
 activation_output = tf.sigmoid(linear_composition)
 
-# match this with the loss show during fit
+# match this with the loss shown during fit
 print(f"calculated loss value: {10*(y - activation_output)}")
