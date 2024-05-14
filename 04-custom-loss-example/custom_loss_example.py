bd3a49c7461423685e538495ae2402294031c5d9
Pranjall Kumar <pranjallk1995@gmail.com>
2024-05-14 11:52:22 +0530

    Update custom_loss_example.py

--- 04-custom-loss-example/custom_loss_example.py
+++ 04-custom-loss-example/custom_loss_example.py
@@ -1,4 +1,3 @@
-import json
 import tensorflow as tf
 
 from tensorflow.keras.layers import Dense
@@ -11,7 +10,7 @@ def custom_loss(y_true, y_pred, extra_parameter=10):
 model = tf.keras.Sequential()
 model.add(
     Dense(
-        units=1, activation='sigmoid', input_shape=(2,),                                           # Notice the input shape
+        units=1, activation='sigmoid', input_shape=(3,),                                               # notice the input shape based on the feature space below
         kernel_initializer='glorot_normal', bias_initializer='glorot_normal'
     )
 )
@@ -23,8 +22,8 @@ print(model.summary())
 model.compile(loss=custom_loss)
 
 # train model
-x = tf.convert_to_tensor([[1., 1.]])                                                              # Notice the x shape compared to x
-y = tf.convert_to_tensor([0.])
+x = tf.convert_to_tensor([[1., 1., 2.], [2., 2., 1.]])                                                 # notice the feature space shape
+y = tf.convert_to_tensor([[0.], [1.]])
 
 model.fit(x, y)
 
@@ -32,8 +31,15 @@ model.fit(x, y)
 weights = tf.convert_to_tensor(model.layers[0].get_weights()[0])
 bias = tf.convert_to_tensor(model.layers[0].get_weights()[1])
 
-linear_composition = weights[0]*x[0][0] + weights[1]*x[0][1] + bias
-activation_output = tf.sigmoid(linear_composition)
+""" input 1: """
+linear_composition_1 = weights[0]*x[0][0] + weights[1]*x[0][1] + weights[2]*x[0][2] + bias
+activation_output_1 = tf.sigmoid(linear_composition_1)
+
+""" input 2: """
+linear_composition_2 = weights[0]*x[1][0] + weights[1]*x[1][1] + weights[2]*x[1][2] + bias
+activation_output_2 = tf.sigmoid(linear_composition_2)
+
+calculated_ypred = tf.convert_to_tensor([activation_output_1, activation_output_2])
 
 # match this with the loss shown during fit
-print(f"calculated loss value: {10*(y - activation_output)}")
+print(f"calculated loss value: {tf.reduce_mean(10*(y - calculated_ypred))}")                            # notice that mean is taken by default
