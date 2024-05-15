dbc3cd72e5cc5f6e695ea8cee6866e9a8662b9eb
Pranjall Kumar <pranjallk1995@gmail.com>
2024-05-15 16:36:41 +0530

    Update custom_loss_example.py

--- 04-custom-loss-example/custom_loss_example.py
+++ 04-custom-loss-example/custom_loss_example.py
@@ -10,7 +10,7 @@ def custom_loss(y_true, y_pred, extra_parameter=10):
 model = tf.keras.Sequential()
 model.add(
     Dense(
-        units=1, activation='sigmoid', input_shape=(3,),                                               # notice the input shape based on the feature space below
+        units=2, activation='sigmoid', input_shape=(3,),                                                        # notice the input shape as compare to feature space
         kernel_initializer='glorot_normal', bias_initializer='glorot_normal'
     )
 )
@@ -22,24 +22,25 @@ print(model.summary())
 model.compile(loss=custom_loss)
 
 # train model
-x = tf.convert_to_tensor([[1., 1., 2.], [2., 2., 1.]])                                                 # notice the feature space shape
-y = tf.convert_to_tensor([[0.], [1.]])
+x = tf.convert_to_tensor([[1., 1., 2.], [2., 2., 1.]])
+y = tf.convert_to_tensor([[0., 1.], [1., 0.]])
 
 model.fit(x, y)
 
 # check:
-weights = tf.convert_to_tensor(model.layers[0].get_weights()[0])
+weights = tf.convert_to_tensor(model.layers[0].get_weights()[0])                                                
 bias = tf.convert_to_tensor(model.layers[0].get_weights()[1])
 
-""" input 1: """
-linear_composition_1 = weights[0]*x[0][0] + weights[1]*x[0][1] + weights[2]*x[0][2] + bias
-activation_output_1 = tf.sigmoid(linear_composition_1)
+network_output = []
+for feature_vector in x:
+    feature_output = []
+    for neuron in range(2):
+        linear_combination = tf.reduce_sum(weights[:, neuron]*feature_vector) + bias[neuron]
+        activation_ouptut = tf.sigmoid(linear_combination)
+        feature_output.append(activation_ouptut)
+    network_output.append(feature_output)
 
-""" input 2: """
-linear_composition_2 = weights[0]*x[1][0] + weights[1]*x[1][1] + weights[2]*x[1][2] + bias
-activation_output_2 = tf.sigmoid(linear_composition_2)
-
-calculated_ypred = tf.convert_to_tensor([activation_output_1, activation_output_2])
+calculated_ypred = tf.convert_to_tensor(network_output)
 
 # match this with the loss shown during fit
-print(f"calculated loss value: {tf.reduce_mean(10*(y - calculated_ypred))}")                            # notice that mean is taken by default
+print(f"calculated loss value: {tf.reduce_mean(10*(y - calculated_ypred))}")                                    # notice that mean is take by default.
